ffc7b36a7cf9a51f416af6c93feb9f0666d87088
mult <mult@program>
2021-08-01 02:00:25 +0300

    from program.sh i remove no-history push

--- program.sh
+++ program.sh
@@ -44,15 +44,4 @@ server(){
     shadow server
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am mult program"
-  git push -f -u origin main
-}
-
 "$@"
