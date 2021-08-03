ea40f1c59a9a03608dd33bfee344e1d7d4a45376
mult <mult@program>
2021-08-03 21:35:32 +0300

    i revert "i use genie library for creating uberjar"
    This reverts commit fbd7cb94a62181db83274005064d5e554491ec28.

--- program.sh
+++ program.sh
@@ -17,13 +17,4 @@ main(){
     -M -m mult.main
 }
 
-uberjar(){
-  clj \
-    -X:uberjar genie.core/process \
-    :uberjar-name out/mult.standalone.jar \
-    :main-ns mult.main
-  mkdir -p out/jpackage-input
-  mv out/mult.standalone.jar out/jpackage-input/
-}
-
 "$@"
