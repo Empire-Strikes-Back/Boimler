7b72125491fd56661713ac8cb28bf8e2e232d847
mult <mult@program>
2021-08-03 13:55:35 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -26,4 +26,31 @@ uberjar(){
   mv out/mult.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting build..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar mult.standalone.jar \
+    --name "mult" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments mult.main \
+    --app-version "1" \
+    $J_ARG
+  
+}
+
 "$@"
